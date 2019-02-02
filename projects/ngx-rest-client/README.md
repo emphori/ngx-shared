6d4b2bdf2ddf9a33755198cc052573eafc1a7a2f
Iain Reid <iainreid382@gmail.com>
2019-02-02 16:25:10 +0000

    Expose Auth interceptor

--- projects/ngx-rest-client/README.md
+++ projects/ngx-rest-client/README.md
@@ -2,3 +2,5 @@ Ngx Rest Client
 ---
 
 A reactive REST client
+
+![](https://img.shields.io/npm/v/@emphori/ngx-rest-client.svg?style=flat-square)
--- projects/ngx-rest-client/src/public_api.ts
+++ projects/ngx-rest-client/src/public_api.ts
@@ -3,5 +3,6 @@
  */
 
 export * from './lib/rest-client.module';
+export * from './lib/interceptors/api-auth.interceptor';
 export * from './lib/services/api-auth.service';
 export * from './lib/services/api-client.service';
